106919907fcd572401379029d82cd8d57d8f5a9e
YIWEI <f1f3f@qq.com>
2018-03-07 17:33:43 +0800

    Update UserMapper.java
    修改注释为中文

--- MyBatisTest/src/com/shiyanlou/mybatis/mapper/UserMapper.java
+++ MyBatisTest/src/com/shiyanlou/mybatis/mapper/UserMapper.java
@@ -1,49 +1,44 @@
-package com.shiyanlou.mybatis.mapper;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE mapper PUBLIC "-//mybatis.org/DTD Mapper 3.0" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+<mapper namespace="com.shiyanlou.mybatis.mapper.UserMapper">
+    <!-- 自定义返回结果集 -->
+    <resultMap id="userMap" type="User">
+        <id property="id" column="id" javaType="int"></id>
+        <result property="username" column="username" javaType="String"></result>
+        <result property="password" column="password" javaType="String"></result>
+        <result property="sex" column="sex" javaType="String"></result>
+        <result property="address" column="address" javaType="String"></result>
+    </resultMap>
 
-import java.util.List;
+    <!-- 定义 SQL 语句，其中 id 需要和接口中的方法名一致 -->
+    <!-- useGeneratedKeys：实现自动生成主键 -->
+    <!-- keyProperty： 唯一标记一个属性 -->
+    <!-- parameterType 指明查询时使用的参数类型，resultType 指明查询返回的结果集类型 -->
+    <insert id="insertUser" useGeneratedKeys="true" keyProperty="id">
+        insert into user (username,password,sex,address) values
+        (#{username},#{password},#{sex},#{address})
+    </insert>
 
-import com.shiyanlou.mybatis.model.User;
+    <update id="updateUser"  parameterType="User">
+        update user set
+        address=#{address} where
+        id=#{id}
+    </update>
 
-public interface UserMapper {
+    <delete id="deleteUser" parameterType="int">
+        delete from user where
+        id=#{id}
+    </delete>
 
-    /*
-     * xinzeng yonghu
-     * @param user
-     * @return
-     * @throws Exception
-     */
-    public int insertUser(User user) throws Exception;
+    <!-- 如未为 Java Bean 起类别名，resultType="com.shiyanlou.mybatis.model.User" -->
 
-    /*
-     * ????
-     * @param user
-     * @param id
-     * @return
-     * @throws Exception
-     */
-    public int updateUser(User user) throws Exception;
+    <!-- 使用resultType时，一定要保证，你属性名与字段名相同；如果不相同，就使用resultMap -->
+    <select id="selectUserById" parameterType="int" resultType="User">
+        select * from user where id=#{id}
+    </select>
 
-    /*
-     * ????
-     * @param id
-     * @return
-     * @throws Exception
-     */
-    public int deleteUser(Integer id) throws Exception;
+    <select id="selectAllUser" resultMap="userMap">
+        select * from user
+    </select>
 
-    /*
-     * ??id??????
-     * @param id
-     * @return
-     * @throws Exception
-     */
-    public User selectUserById(Integer id) throws Exception;
-
-    /*
-     * ?????????
-     * @return
-     * @throws Exception
-     */
-    public List<User> selectAllUser() throws Exception;
-}
-
+</mapper>
